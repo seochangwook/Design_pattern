ca8a44732d55814fdf214671b3300a7bd1b74763
seochangwook <scw05313315@gmail.com>
2019-02-06 22:48:21 +0900

    builder pattern fix

--- src/builder/BuilderExample.java
+++ src/builder/BuilderExample.java
@@ -1,7 +1,12 @@
 package builder;
 
+/*
+Builder Pattern
+1) 정의
+	복합 객체의 생성 과정과 표현 방법을 분리하여 동일한 생성 절차에서 서로 다른 표현 결과를 만들 수 있게 하는 패턴
+ */
 public class BuilderExample {
 	public static void main(String args[]) {
-		System.out.println("test");
+		
 	}
 }
